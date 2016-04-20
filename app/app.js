bc450fddc79a897783e03e74cb271326bd98a52b
Mike Adams <michaelglenadams@gmail.com>
2016-04-20 11:59:30 -0700

    add index hero list

---
+++ app/app.js
@@ -0,0 +1,18 @@
+angular.module('hero-app', [])
+
+angular.module('hero-app')
+.controller('ListCtrl', function($scope, $http){
+
+	$scope.newHero = {};
+
+	$http.get('/data/products.json')
+		.then(function(res){
+			$scope.heros = res.data
+		})
+
+	$scope.addHero = function (hero){
+		$scope.heros.push(hero);
+		$scope.newHero = {};
+	}
+
+})
